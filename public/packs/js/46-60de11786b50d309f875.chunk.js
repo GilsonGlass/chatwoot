00fd39350aeef6d498bb0a58a9991e70abb5ad8a
EvolutionDesenvERP <76484412+EvolutionDesenvERP@users.noreply.github.com>
2023-04-20 14:44:14 -0300

    commit
    commit

---
+++ public/packs/js/46-60de11786b50d309f875.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[46],{3050:function(n,t,e){"use strict";e.r(t);var a=e(1),l=Object(a.a)({},(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",[n._v("Component to list articles in a category in a portal")])}),[],!1,null,null,null);t.default=l.exports}}]);
+//# sourceMappingURL=46-60de11786b50d309f875.chunk.js.map
